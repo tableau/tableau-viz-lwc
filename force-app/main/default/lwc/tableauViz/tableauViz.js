6ac7509ff4f3e950e663d88dc0d111d34a44df6e
Geraldine Zanolli <zanolli.geraldine@gmail.com>
2020-04-03 11:53:54 -0700

    Pushing enhancements proposed by Philippe

--- force-app/main/default/lwc/tableauViz/tableauViz.js
+++ force-app/main/default/lwc/tableauViz/tableauViz.js
@@ -1,56 +1,40 @@
-import { LightningElement, api, wire } from 'lwc';
-import { getRecord } from 'lightning/uiRecordApi';
-import { loadScript } from 'lightning/platformResourceLoader';
+import { LightningElement, api, wire } from "lwc";
+import { getRecord } from "lightning/uiRecordApi";
+import { loadScript } from "lightning/platformResourceLoader";
 
-import tableauJSAPI from '@salesforce/resourceUrl/tableauJSAPI';
-
-const FIELDS = [
-    'Contact.Name',
-    'Contact.Title',
-    'Contact.Phone',
-    'Contact.Email',
-];
+import tableauJSAPI from "@salesforce/resourceUrl/tableauJSAPI";
 
 export default class TableauViz extends LightningElement {
-    privateStyle;
-    @api recordId
-    @api flexipageRegionWidth
-    @api vizURL
-    @api hideTabs
-    @api hideToolbar
-    @api listenToMarksSelection
-    @api listenToFilterChanged
-    @api filter
-    @api objectApiName;
+  privateStyle;
+  @api recordId;
+  @api flexipageRegionWidth;
+  @api vizURL;
+  @api hideTabs;
+  @api hideToolbar;
+  @api filter;
+  @api objectApiName;
 
-    @wire(getRecord, { recordId: '$recordId', fields: FIELDS })
-    account;
+  @wire(getRecord, { recordId: "$recordId" })
+  account;
 
+  async renderedCallback() {
+    await loadScript(this, tableauJSAPI);
 
-    async renderedCallback() {
-        await loadScript(this, tableauJSAPI);
-        
-        
-        if (this.vizURL !== "--None--") {
-            console.log(this.flexipageRegionWidth);
-            console.log(this.objectApiName);
-            
-            let containerDiv = this.template.querySelector('div');
-            console.log(this.filter);
-            let vizToLoad;
-            
-            if(this.filter == true){
-                vizToLoad = this.vizURL+"?"+this.objectApiName+" ID="+this.recordId;
-            }
-            else{
-                vizToLoad = this.vizURL;
-            }
-            
-            const options = {
-                    hideTabs: this.hideTabs,
-                    hideToolbar: this.hideToolbar  
-                };
-            this.viz = new tableau.Viz(containerDiv, vizToLoad, options);
-        }
+    let containerDiv = this.template.querySelector("div");
+
+    let vizToLoad;
+
+    if (this.filter == true) {
+      vizToLoad =
+        this.vizURL + "?" + this.objectApiName + " ID=" + this.recordId;
+    } else {
+      vizToLoad = this.vizURL;
     }
+
+    const options = {
+      hideTabs: this.hideTabs,
+      hideToolbar: this.hideToolbar
+    };
+    this.viz = new tableau.Viz(containerDiv, vizToLoad, options);
+  }
 }
