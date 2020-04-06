9fa4aad2be90d1530fe3ce6abebd132f5af427ee
pozil <philippe.ozil@gmail.com>
2020-04-06 11:43:18 +0200

    Simplified code and removed unused vars

--- force-app/main/default/lwc/tableauViz/tableauViz.js
+++ force-app/main/default/lwc/tableauViz/tableauViz.js
@@ -1,40 +1,31 @@
-import { LightningElement, api, wire } from "lwc";
-import { getRecord } from "lightning/uiRecordApi";
-import { loadScript } from "lightning/platformResourceLoader";
+import { LightningElement, api } from 'lwc';
+import { loadScript } from 'lightning/platformResourceLoader';
 
-import tableauJSAPI from "@salesforce/resourceUrl/tableauJSAPI";
+import tableauJSAPI from '@salesforce/resourceUrl/tableauJSAPI';
 
 export default class TableauViz extends LightningElement {
-  privateStyle;
-  @api recordId;
-  @api flexipageRegionWidth;
-  @api vizURL;
-  @api hideTabs;
-  @api hideToolbar;
-  @api filter;
-  @api objectApiName;
+    @api objectApiName;
+    @api recordId;
+    @api vizURL;
+    @api hideTabs;
+    @api hideToolbar;
+    @api filter;
 
-  @wire(getRecord, { recordId: "$recordId" })
-  account;
+    async renderedCallback() {
+        await loadScript(this, tableauJSAPI);
 
-  async renderedCallback() {
-    await loadScript(this, tableauJSAPI);
+        const containerDiv = this.template.querySelector('div');
 
-    let containerDiv = this.template.querySelector("div");
+        let vizToLoad = this.vizURL;
+        if (this.filter && this.objectApiName) {
+            vizToLoad += `?${this.objectApiName}&ID=${this.recordId}`;
+        }
 
-    let vizToLoad;
-
-    if (this.filter == true) {
-      vizToLoad =
-        this.vizURL + "?" + this.objectApiName + " ID=" + this.recordId;
-    } else {
-      vizToLoad = this.vizURL;
+        const options = {
+            hideTabs: this.hideTabs,
+            hideToolbar: this.hideToolbar
+        };
+        // eslint-disable-next-line no-undef
+        this.viz = new tableau.Viz(containerDiv, vizToLoad, options);
     }
-
-    const options = {
-      hideTabs: this.hideTabs,
-      hideToolbar: this.hideToolbar
-    };
-    this.viz = new tableau.Viz(containerDiv, vizToLoad, options);
-  }
 }
