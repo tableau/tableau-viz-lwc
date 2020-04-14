33cd75e6d4dc365571506bbb675f8b177c1443f5
Geraldine Zanolli <zanolli.geraldine@gmail.com>
2020-04-14 13:35:21 -0700

    Add advanced filtering

--- force-app/main/default/lwc/tableauViz/tableauViz.js
+++ force-app/main/default/lwc/tableauViz/tableauViz.js
@@ -1,6 +1,6 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
 import { loadScript } from 'lightning/platformResourceLoader';
-
+import { getRecord } from 'lightning/uiRecordApi';
 import tableauJSAPI from '@salesforce/resourceUrl/tableauJSAPI';
 
 export default class TableauViz extends LightningElement {
@@ -11,21 +11,55 @@ export default class TableauViz extends LightningElement {
     @api hideToolbar;
     @api filter;
     @api height;
-
+    @api filter_name;
+    @api sf_advanced_filter;
     viz;
+    sf_value;
+
+    // Extract current picklist value for this record
+    @wire(getRecord, {
+        recordId: '$recordId',
+        fields: '$sf_advanced_filter'
+    })
+    getRecord({ error, data }) {
+        if (data) {
+            const fieldName = this.getFieldName();
+            this.sf_value = data.fields[fieldName].value;
+        } else if (error) {
+            this.errorMessage = `Failed to retrieve record data. ${this.reduceErrors(
+                error
+            )}`;
+        }
+    }
+
+    getFieldName() {
+        return this.sf_advanced_filter.substring(
+            this.sf_advanced_filter.indexOf('.') + 1
+        );
+    }
 
     async renderedCallback() {
         await loadScript(this, tableauJSAPI);
 
         const containerDiv = this.template.querySelector('div');
         containerDiv.style.height = this.height + 'px';
-
+        let filterName = `${this.objectApiName}%20ID`;
         let vizToLoad = this.vizURL;
+
+        //In context filtering
         if (this.filter && this.objectApiName) {
             if (vizToLoad.includes('?')) {
-                vizToLoad += `&${this.objectApiName}%20ID=${this.recordId}`;
+                vizToLoad += `&${filterName}=${this.recordId}`;
             } else {
-                vizToLoad += `?${this.objectApiName}%20ID=${this.recordId}`;
+                vizToLoad += `?${filterName}=${this.recordId}`;
+            }
+        }
+        //Additional Filtering
+        if (this.sf_value && this.objectApiName) {
+            if (vizToLoad.includes('?')) {
+                vizToLoad += `&${this.filter_name}=${this.sf_value}`;
+            } else {
+                vizToLoad += `?${this.this.filter_name}=${this.sf_value}`;
             }
         }
 
