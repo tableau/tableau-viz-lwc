ff587ea848e9808bf192c97f1e516f4c68dc5c04
Geraldine Zanolli <zanolli.geraldine@gmail.com>
2020-04-24 16:02:39 -0700

    Updated metadata help

--- force-app/main/default/lwc/tableauViz/tableauViz.js
+++ force-app/main/default/lwc/tableauViz/tableauViz.js
@@ -3,6 +3,9 @@ import { loadScript } from 'lightning/platformResourceLoader';
 import { getRecord } from 'lightning/uiRecordApi';
 import tableauJSAPI from '@salesforce/resourceUrl/tableauJSAPI';
 
+
+
+
 export default class TableauViz extends LightningElement {
     @api objectApiName;
     @api recordId;
@@ -16,7 +19,7 @@ export default class TableauViz extends LightningElement {
     @track record;
     viz;
     sf_value;
-
+    
     // Extract current picklist value for this record
     @wire(getRecord, {
         recordId: '$recordId',
@@ -28,27 +31,23 @@ export default class TableauViz extends LightningElement {
             const fieldName = this.getFieldName();
             this.sf_value = data.fields[fieldName].value;
         } else if (error) {
-            this.errorMessage = `Failed to retrieve record data. ${this.reduceErrors(
-                error
-            )}`;
+            this.errorMessage = `Failed to retrieve record data. ${this.reduceErrors(error)}`;
         }
     }
 
     getFieldName() {
-        return this.sfAdvancedFilter.substring(
-            this.sfAdvancedFilter.indexOf('.') + 1
-        );
-    }
+        return this.sfAdvancedFilter.substring(this.sfAdvancedFilter.indexOf('.') +1);
+    }    
 
     async renderedCallback() {
         await loadScript(this, tableauJSAPI);
-
-        const containerDiv = this.template.querySelector('div');
-        if (!this.sfAdvancedFilter || !this.filterName) {
-            this.record = 'No Filter';
+        
+        if (!this.sfAdvancedFilter || !this.filterName){
+            this.record ="No Filter";
         }
-
-        if (containerDiv) {
+        const containerDiv = this.template.querySelector('div');
+        
+        if(containerDiv){
             containerDiv.style.height = this.height + 'px';
             const filterName = `${this.objectApiName}%20ID`;
             let vizToLoad = this.vizURL;
@@ -73,12 +72,14 @@ export default class TableauViz extends LightningElement {
             const options = {
                 hideTabs: this.hideTabs,
                 hideToolbar: this.hideToolbar,
-                height: this.height + 'px',
+                height: this.height+'px',
                 width: '100%'
             };
-
+            
             // eslint-disable-next-line no-undef
             this.viz = new tableau.Viz(containerDiv, vizToLoad, options);
+        
         }
     }
+    
 }
