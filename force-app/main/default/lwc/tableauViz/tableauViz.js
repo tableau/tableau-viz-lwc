98e3608c4f79dfb33bc232c1f0e38ac1e88f56ac
Geraldine Zanolli <zanolli.geraldine@gmail.com>
2020-04-29 15:17:07 -0700

    Add illustrations and clean code

--- force-app/main/default/lwc/tableauViz/tableauViz.js
+++ force-app/main/default/lwc/tableauViz/tableauViz.js
@@ -1,4 +1,4 @@
-import { LightningElement, api, wire, track } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
 import { loadScript } from 'lightning/platformResourceLoader';
 import { getRecord } from 'lightning/uiRecordApi';
 import tableauJSAPI from '@salesforce/resourceUrl/tableauJSAPI';
@@ -13,24 +13,43 @@ export default class TableauViz extends LightningElement {
     @api height;
     @api filterName;
     @api sfAdvancedFilter;
-    @track record;
     viz;
-    sf_value;
+    sfValue;
+    errorMessage;
+    flag;
 
-    // Extract current picklist value for this record
     @wire(getRecord, {
         recordId: '$recordId',
         fields: '$sfAdvancedFilter'
     })
     getRecord({ error, data }) {
-        if (data) {
-            this.record = data;
+        if (this.sfAdvancedFilter) {
             const fieldName = this.getFieldName();
-            this.sf_value = data.fields[fieldName].value;
-        } else if (error) {
-            this.errorMessage = `Failed to retrieve record data. ${this.reduceErrors(
-                error
-            )}`;
+            if (data.fields[fieldName]) {
+                this.sfValue = data.fields[fieldName].value;
+            } else if (error) {
+                this.errorMessage = `Failed to retrieve record data. ${this.reduceErrors(
+                    error
+                )}`;
+            }
+        }
+    }
+
+    get isVizDisplayed() {
+        this.createErrorMessage();
+        this.flag =
+            (!this.validURL(this.vizURL) && !this.sfAdvancedFilter) ||
+            this.sfValue;
+        return this.flag;
+    }
+
+    createErrorMessage() {
+        if (this.validURL(this.vizURL)) {
+            this.errorMessage = 'Invalid Viz URL';
+        } else if (!this.sfValue) {
+            this.errorMessage = 'Invalid Salesforce qualified Field Name';
+        } else {
+            this.errorMessage = 'Invalid Input';
         }
     }
 
@@ -40,19 +59,66 @@ export default class TableauViz extends LightningElement {
         );
     }
 
+    validURL(str) {
+        var pattern = new RegExp(
+            '^(https?:\\/\\/)?' + // protocol
+            '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|' + // domain name
+            '((\\d{1,3}\\.){3}\\d{1,3}))' + // ip (v4) address
+            '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + //port
+            '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+                '(\\#[-a-z\\d_]*)?$',
+            'i'
+        );
+        return pattern.test(str);
+    }
+
+    reduceErrors(errors) {
+        if (!Array.isArray(errors)) {
+            errors = [errors];
+        }
+
+        return (
+            errors
+                // Remove null/undefined items
+                .filter(error => !!error)
+                // Extract an error message
+                .map(error => {
+                    // UI API read errors
+                    if (Array.isArray(error.body)) {
+                        return error.body.map(e => e.message);
+                    }
+                    // UI API DML, Apex and network errors
+                    else if (
+                        error.body &&
+                        typeof error.body.message === 'string'
+                    ) {
+                        return error.body.message;
+                    }
+                    // JS errors
+                    else if (typeof error.message === 'string') {
+                        return error.message;
+                    }
+                    // Unknown error shape so try HTTP status text
+                    return error.statusText;
+                })
+                // Flatten
+                .reduce((prev, curr) => prev.concat(curr), [])
+                // Remove empty strings
+                .filter(message => !!message)
+        );
+    }
     async renderedCallback() {
         await loadScript(this, tableauJSAPI);
         const containerDiv = this.template.querySelector('div');
 
-        if (containerDiv && this.vizURL) {
+        if (this.flag) {
             //Defining the height of the div
             containerDiv.style.height = this.height + 'px';
 
             //Creating a URL object
-            console.log(this.vizURL);
-            let vizToLoad = new URL(this.vizURL);
-            //Getting Width of the viez
-            let vizWidth = containerDiv.offsetHeight;
+            const vizToLoad = new URL(this.vizURL);
+            //Getting Width of the viz
+            const vizWidth = containerDiv.offsetHeight;
 
             //Define size of the viz
             vizToLoad.searchParams.append('size', vizWidth + ',' + this.height);
@@ -64,8 +130,8 @@ export default class TableauViz extends LightningElement {
             }
 
             //Additional Filtering
-            if (this.sf_value && this.filterName) {
-                vizToLoad.searchParams.append(this.filterName, this.sf_value);
+            if (this.sfValue && this.filterName) {
+                vizToLoad.searchParams.append(this.filterName, this.sfValue);
             }
 
             let vizURLString = vizToLoad.toString();
