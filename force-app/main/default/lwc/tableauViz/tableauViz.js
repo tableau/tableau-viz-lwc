8de1a11b020056c4d8ceb9d06edf22ac8019ab72
Geraldine Zanolli <zanolli.geraldine@gmail.com>
2020-05-01 10:38:01 -0700

    Remove advanced filtering

--- force-app/main/default/lwc/tableauViz/tableauViz.js
+++ force-app/main/default/lwc/tableauViz/tableauViz.js
@@ -1,6 +1,5 @@
-import { LightningElement, api, wire } from 'lwc';
+import { LightningElement, api } from 'lwc';
 import { loadScript } from 'lightning/platformResourceLoader';
-import { getRecord } from 'lightning/uiRecordApi';
 import tableauJSAPI from '@salesforce/resourceUrl/tableauJSAPI';
 
 export default class TableauViz extends LightningElement {
@@ -11,53 +10,23 @@ export default class TableauViz extends LightningElement {
     @api hideToolbar;
     @api filter;
     @api height;
-    @api filterName;
-    @api sfAdvancedFilter;
     viz;
-    sfValue;
     errorMessage;
     flag;
     vizToLoad;
 
-    @wire(getRecord, {
-        recordId: '$recordId',
-        fields: '$sfAdvancedFilter'
-    })
-    getRecord({ error, data }) {
-        if (this.sfAdvancedFilter) {
-            const fieldName = this.getFieldName();
-            if (data.fields[fieldName]) {
-                this.sfValue = data.fields[fieldName].value;
-            } else if (error) {
-                this.errorMessage = `Failed to retrieve record data. ${this.reduceErrors(
-                    error
-                )}`;
-            }
-        }
-    }
-
     get isVizDisplayed() {
         this.createErrorMessage();
-        this.flag =
-            (this.validURL(this.vizURL) && !this.sfAdvancedFilter) ||
-            this.sfValue;
+        this.flag = this.validURL(this.vizURL);
         return this.flag;
     }
 
     createErrorMessage() {
         if (!this.validURL(this.vizURL)) {
             this.errorMessage = 'Invalid Viz URL';
-        } else if (!this.sfValue) {
-            this.errorMessage = 'Invalid Salesforce qualified Field Name';
         } else {
             this.errorMessage = 'Invalid Input';
         }
-    }
-
-    getFieldName() {
-        return this.sfAdvancedFilter.substring(
-            this.sfAdvancedFilter.indexOf('.') + 1
-        );
     }
 
     validURL(str) {
@@ -70,41 +39,6 @@ export default class TableauViz extends LightningElement {
         return true;
     }
 
-    reduceErrors(errors) {
-        if (!Array.isArray(errors)) {
-            errors = [errors];
-        }
-
-        return (
-            errors
-                // Remove null/undefined items
-                .filter(error => !!error)
-                // Extract an error message
-                .map(error => {
-                    // UI API read errors
-                    if (Array.isArray(error.body)) {
-                        return error.body.map(e => e.message);
-                    }
-                    // UI API DML, Apex and network errors
-                    else if (
-                        error.body &&
-                        typeof error.body.message === 'string'
-                    ) {
-                        return error.body.message;
-                    }
-                    // JS errors
-                    else if (typeof error.message === 'string') {
-                        return error.message;
-                    }
-                    // Unknown error shape so try HTTP status text
-                    return error.statusText;
-                })
-                // Flatten
-                .reduce((prev, curr) => prev.concat(curr), [])
-                // Remove empty strings
-                .filter(message => !!message)
-        );
-    }
     async renderedCallback() {
         await loadScript(this, tableauJSAPI);
         const containerDiv = this.template.querySelector('div');
@@ -128,14 +62,6 @@ export default class TableauViz extends LightningElement {
                 this.vizToLoad.searchParams.append(
                     filterNameTab,
                     this.recordId
-                );
-            }
-
-            //Additional Filtering
-            if (this.sfValue && this.filterName) {
-                this.vizToLoad.searchParams.append(
-                    this.filterName,
-                    this.sfValue
                 );
             }
 
